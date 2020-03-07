75f24a13465d59f9b8f11565365fa2b96d8e8bc8
ArbaazAli <arbajali02@gmail.com>
2020-03-07 08:03:29 +0530

    pushing

--- career/views.py
+++ career/views.py
@@ -7,14 +7,38 @@ import re
 def home(request):
     if request.method == "POST":
         selection = request.POST['selections']
-        #remove_sp = re.sub(" ", "", context )
-        # choice = Choice(selections = context)
-        # choice.save()
-       # print(context)
-        #print(remove_sp)
-        #tags = remove_sp.lower().split(',')
-       # print(tags) 
+        print(selection)
+        remove_sp = re.sub(" ", "", context )
+        
+       
+        print(remove_sp)
+        
         #context = {'selection' : selection}
+        diction = {'actor': ['drama', 'acting', 'dancing'],
+           'manager': ['management', 'problemsolving', 'business'],
+           'engineer': ['programming', 'problemsolving', 'teamplay'],
+           'doctor': ['problemsolving', 'teamplay', 'decisionmaking'],
+           'cybersecurity': ['cryptography', 'networking', 'linux'],
+           'hotelmanagement': ['cooking', 'creativity', 'management'],
+           'datascientist': ['statistics', 'analysis', 'machinelearning'],
+           'robotics': ['maths', 'machinelearning', 'programming'],
+            'graphicsdesign': ['animation', 'uiuxdesign', 'creativity'],
+            'law': ['reasoning', 'legalresearch', 'communication'],
+            'teacher': ['communication', 'criticalthinking', 'technicalskills'],
+            'accountant': ['maths', 'finance', 'statistics']
+           }
+ 
+        print(diction.values())
+        tags = remove_sp.lower().split(',')
+        print(tags) 
+        career = set()
+        
+        for key in diction.keys():
+            for skill in tags:
+                if skill in diction[key]:
+                    career.add(key)
+                
+        print(list(career))
         return render(request, 'roadmap.html', context)
 
     else:
